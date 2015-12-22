2e622498fcf095400c71d1734ef847f7dc8e5986
Luz <luzma_87@yahoo.com>
2015-12-22 15:38:50 -0500

    +@faustocv refactorizacion de los mensajes para el controlador + creada taglib de mensajes

--- grails-app/controllers/ec/com/tw/parking/UsuarioController.groovy
+++ grails-app/controllers/ec/com/tw/parking/UsuarioController.groovy
@@ -17,23 +17,23 @@ class UsuarioController extends Shield {
     }
 
     def show_ajax() {
-        if(params.id) {
+        if (params.id) {
             def usuarioInstance = Usuario.get(params.id)
-            if(!usuarioInstance) {
-                render mensajeError('default.not.found.message', params.id)
+            if (!usuarioInstance) {
+                render msgBuilder.noEncontrado(entidad: 'usuario', id: params.id)
             }
             return [usuarioInstance: usuarioInstance]
         } else {
-            render mensajeError('default.not.found.message', params.id)
+            render msgBuilder.noEncontrado(entidad: 'usuario', id: params.id)
         }
     }
 
     def form_ajax() {
         def usuarioInstance = new Usuario()
-        if(params.id) {
+        if (params.id) {
             usuarioInstance = Usuario.get(params.id)
-            if(!usuarioInstance) {
-                render mensajeError('default.not.found.message', params.id)
+            if (!usuarioInstance) {
+                render msgBuilder.noEncontrado(entidad: 'usuario', id: params.id)
                 return
             }
         }
@@ -43,49 +43,36 @@ class UsuarioController extends Shield {
 
     def save_ajax() {
         def usuarioInstance = new Usuario()
-        if(params.id) {
+        if (params.id) {
             usuarioInstance = Usuario.get(params.id)
-            if(!usuarioInstance) {
-                render mensajeError('default.not.found.message', params.id)
+            if (!usuarioInstance) {
+                render msgBuilder.noEncontrado(entidad: 'usuario', id: params.id)
                 return
             }
         }
         usuarioInstance.properties = params
-        if(!usuarioInstance.save(flush: true)) {
-            render mensajeError('default.not.saved.message', params.id) + renderErrors(bean: usuarioInstance)
+        if (!usuarioInstance.save(flush: true)) {
+            render msgBuilder.noGuardado(entidad: 'usuario', id: params.id)
             return
         }
-        render mensajeExito('default.saved.message', params.id)
+        render msgBuilder.guardado(entidad: 'usuario', id: params.id)
     }
 
     def delete_ajax() {
-        if(params.id) {
+        if (params.id) {
             def usuarioInstance = Usuario.get(params.id)
             if (!usuarioInstance) {
-                render mensajeError('default.not.found.message', params.id)
+                render msgBuilder.noEncontrado(entidad: 'usuario', id: params.id)
                 return
             }
             try {
                 usuarioInstance.delete(flush: true)
-                render mensajeExito('default.deleted.message', params.id)
+                render msgBuilder.eliminado(entidad: 'usuario', id: params.id)
             } catch (DataIntegrityViolationException e) {
-                render mensajeError('default.delete.error.message', params.id)
+                render msgBuilder.noEliminado(entidad: 'usuario', id: params.id)
             }
         } else {
-            render mensajeError('default.not.found.message', params.id)
+            render msgBuilder.noEncontrado(entidad: 'usuario', id: params.id)
         }
-    }
-
-    private String mensajeConCodigo(tipo, codigo, id) {
-        def separador = "*"
-        return tipo + separador + message(code: codigo, args: [message(code: 'usuario.label', default: 'Usuario'), id])
-    }
-
-    private String mensajeError(codigo, id) {
-        mensajeConCodigo('ERROR', codigo, id)
-    }
-
-    private String mensajeExito(codigo, id) {
-        mensajeConCodigo('SUCCESS', codigo, id)
     }
 }
---
+++ grails-app/taglib/ec/com/tw/parking/MensajesTagLib.groovy
@@ -0,0 +1,67 @@
+package ec.com.tw.parking
+
+class MensajesTagLib {
+    static defaultEncodeAs = [taglib: 'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+
+    static namespace = "msgBuilder"
+
+    def mensajeConCodigo = { attrs ->
+        def tipo = attrs.tipo
+        def codigo = attrs.codigo
+        def id = attrs.id
+        def entidad = message(code: attrs.entidad + '.label')
+        def separador = attrs.separador ?: "*"
+        out << tipo << separador << message(code: codigo, args: [entidad, id])
+    }
+
+    def error = { attrs ->
+        def codigo = attrs.codigo
+        def id = attrs.id
+        def entidad = attrs.entidad
+        out << mensajeConCodigo(tipo: "ERROR", codigo: codigo, id: id, entidad: entidad)
+    }
+
+    def exito = { attrs ->
+        def codigo = attrs.codigo
+        def id = attrs.id
+        def entidad = attrs.entidad
+        out << mensajeConCodigo(tipo: "SUCCESS", codigo: codigo, id: id, entidad: entidad)
+    }
+
+    def noEncontrado = { attrs ->
+        def codigo = 'default.not.found.message'
+        def id = attrs.id
+        def entidad = attrs.entidad
+        out << error(codigo: codigo, id: id, entidad: entidad)
+    }
+
+    def noGuardado = {attrs ->
+        def codigo = 'default.not.saved.message'
+        def id = attrs.id
+        def entidad = attrs.entidad
+        out << error(codigo: codigo, id: id, entidad: entidad)
+    }
+
+    def guardado = {attrs ->
+        def codigo = 'default.saved.message'
+        def id = attrs.id
+        def entidad = attrs.entidad
+        out << exito(codigo: codigo, id: id, entidad: entidad)
+    }
+
+    def noEliminado = {attrs ->
+        def codigo = 'default.not.deleted.message'
+        def id = attrs.id
+        def entidad = attrs.entidad
+        out << error(codigo: codigo, id: id, entidad: entidad)
+    }
+
+    def eliminado = {attrs ->
+        def codigo = 'default.deleted.message'
+        def id = attrs.id
+        def entidad = attrs.entidad
+        out << exito(codigo: codigo, id: id, entidad: entidad)
+    }
+
+}
