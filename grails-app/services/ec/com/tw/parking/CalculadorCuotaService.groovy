9f20ef45a7200d5de40f27ec2d681aaa8fc90bac
Luz <luzma_87@yahoo.com>
2016-01-06 14:10:04 -0500

    +@faustocv agregando parametros al calculador de cuota

--- grails-app/services/ec/com/tw/parking/CalculadorCuotaService.groovy
+++ grails-app/services/ec/com/tw/parking/CalculadorCuotaService.groovy
@@ -2,10 +2,17 @@ package ec.com.tw.parking
 
 class CalculadorCuotaService {
 
-    def calcularCuota() {
+    def calcularCuota(puestosFaltantes, precio) {
         def cantidadUsuarios = Usuario.countByEstaActivo(true)
         def puestos = Puesto.list()
         def sumaTotal = puestos.sum { it.precio }
+        if (puestosFaltantes > 0) {
+            sumaTotal += (puestosFaltantes * precio)
+        }
         return sumaTotal / cantidadUsuarios
+    }
+
+    def calcularCuota() {
+        return calcularCuota(0, 0)
     }
 }
--- test/unit/ec/com/tw/parking/CalculadorCuotaServiceSpec.groovy
+++ test/unit/ec/com/tw/parking/CalculadorCuotaServiceSpec.groovy
@@ -4,31 +4,49 @@ import ec.com.tw.parking.builders.PuestoBuilder
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
+import static ec.com.tw.parking.helpers.RandomUtilsHelpers.getRandomDouble
 import static ec.com.tw.parking.helpers.RandomUtilsHelpers.getRandomInt
 
 @TestFor(CalculadorCuotaService)
 class CalculadorCuotaServiceSpec extends Specification {
 
+    def cantidad
+    def puestos
+    def precioTotal
+
     def setup() {
+        cantidad = getRandomInt(1, 10)
+        puestos = []
+        cantidad.times {
+            puestos += new PuestoBuilder().crear()
+        }
+
+        GroovyMock(Puesto, global: true)
+        Puesto.list() >> puestos
+        precioTotal = puestos.sum { it.precio }
     }
 
     void "Debe calcular la cuota por usuario del servicio de parqueadero"() {
         setup:
-        def cantidad = getRandomInt(10)
-        def puestos = []
-        cantidad.times {
-            puestos += new PuestoBuilder().crear()
-        }
+        def cuota = precioTotal / cantidad
         GroovyMock(Usuario, global: true)
         Usuario.countByEstaActivo(true) >> cantidad
 
-        GroovyMock(Puesto, global: true)
-        Puesto.list() >> puestos
-
-        def precioTotal = puestos.sum { it.precio }
-        def cuota = precioTotal / cantidad
-
         expect:
         service.calcularCuota() == cuota
+    }
+
+    void "Debe calcular la cuota tomando en cuenta los puestos faltantes y el precio"() {
+        setup:
+        def puestosFaltantes = getRandomInt(1, 10)
+        def precio = getRandomDouble(100)
+        GroovyMock(Usuario, global: true)
+        Usuario.countByEstaActivo(true) >> cantidad + puestosFaltantes
+
+        def cuotaNueva = (precioTotal + (puestosFaltantes * precio)) / (cantidad + puestosFaltantes)
+
+        expect:
+        service.calcularCuota(puestosFaltantes, precio) == cuotaNueva
+
     }
 }
