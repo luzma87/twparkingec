304e957f503e39e1b909268deb5819ed7cd913be
Luz <luzma_87@yahoo.com>
2016-01-06 10:54:23 -0500

    bootstrap con autos

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -54,9 +54,15 @@ class BootStrap {
 
         if (Usuario.count() == 0) {
             listaUsuariosIniciales().each { datosUsuario ->
-                def usuario = new Usuario(datosUsuario)
+                def usuario = new Usuario(datosUsuario.usuario)
                 if (!usuario.save()) {
                     println "Error al crear usuario: " + usuario.errors
+                } else {
+                    def auto = new Auto(datosUsuario.auto)
+                    auto.usuario = usuario
+                    if (!auto.save()) {
+                        println "Error al crear auto: " + auto.errors
+                    }
                 }
             }
         }
@@ -67,116 +73,212 @@ class BootStrap {
     def listaUsuariosIniciales() {
         return [
             [
-                nombre  : "Carlos Oquendo",
-                email   : "coquendo@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "0721351797"
+                usuario: [
+                    nombre  : "Carlos Oquendo",
+                    email   : "coquendo@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "0721351797"
+                ],
+                auto   : [marca  : "Renault",
+                          modelo : "Logan",
+                          placa  : "PBQ-8392",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Mauricio Murillo",
-                email   : "mmurillo@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1714579305"
+                usuario: [
+                    nombre  : "Mauricio Murillo",
+                    email   : "mmurillo@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1714579305"
+                ],
+                auto   : [marca  : "Chevrolet",
+                          modelo : "Spark",
+                          placa  : "PBO-7783",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Fausto de la Torre",
-                email   : "ftorre@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1710876275"
+                usuario: [
+                    nombre  : "Fausto de la Torre",
+                    email   : "ftorre@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1710876275"
+                ],
+                auto   : [marca  : "Kia",
+                          modelo : "Sportage",
+                          placa  : "PBJ-5195",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Fausto Castañeda",
-                email   : "fcastane@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : true,
-                cedula  : "1718198938"
+                usuario: [
+                    nombre  : "Fausto Castañeda",
+                    email   : "fcastane@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : true,
+                    cedula  : "1718198938"
+                ],
+                auto   : [marca  : "Nissan",
+                          modelo : "Pathfinder",
+                          placa  : "PYM-347",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Paola Jimenez",
-                email   : "pjimenez@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "0703594747"
+                usuario: [
+                    nombre  : "Paola Jimenez",
+                    email   : "pjimenez@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "0703594747"
+                ],
+                auto   : [marca  : "Chevrolet",
+                          modelo : "Grand Vitara",
+                          placa  : "PBF-9129",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Gustavo Eguez",
-                email   : "geguez@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1714060439"
+                usuario: [
+                    nombre  : "Gustavo Eguez",
+                    email   : "geguez@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1714060439"
+                ],
+                auto   : [marca  : "Chevrolet",
+                          modelo : "Grand Vitara 5P",
+                          placa  : "PCO-5724",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Gaby Cortez",
-                email   : "gcortez@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1717360281"
+                usuario: [
+                    nombre  : "Gaby Cortez",
+                    email   : "gcortez@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1717360281"
+                ],
+                auto   : [marca  : "Volkswagen",
+                          modelo : "Gol",
+                          placa  : "PPA-1608",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Felipe Ureta",
-                email   : "fureta@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1707157093"
+                usuario: [
+                    nombre  : "Felipe Ureta",
+                    email   : "fureta@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1707157093"
+                ],
+                auto   : [marca  : "Chevrolet",
+                          modelo : "Vitara 3P",
+                          placa  : "PCF-4689",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Iván Pazmiño",
-                email   : "ipazmino@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1234567898"
+                usuario: [
+                    nombre  : "Iván Pazmiño",
+                    email   : "ipazmino@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1234567898"
+                ],
+                auto   : [marca  : "Chevrolet",
+                          modelo : "Vitara 5P",
+                          placa  : "AAA-111",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Rodrigo Vallejo",
-                email   : "rvallejo@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1716368251"
+                usuario: [
+                    nombre  : "Rodrigo Vallejo",
+                    email   : "rvallejo@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1716368251"
+                ],
+                auto   : [marca  : "Chevrolet",
+                          modelo : "Grand Vitara SZ",
+                          placa  : "AAA-222",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "María Fernanda Escudero",
-                email   : "mescudero@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "0602772220"
+                usuario: [
+                    nombre  : "María Fernanda Escudero",
+                    email   : "mescudero@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "0602772220"
+                ],
+                auto   : [marca  : "Chevrolet",
+                          modelo : "Grand Vitara 5P",
+                          placa  : "PBI-2312",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Viviana Perez",
-                email   : "vperez@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1720984630"
+                usuario: [
+                    nombre  : "Viviana Perez",
+                    email   : "vperez@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1720984630"
+                ],
+                auto   : [marca  : "Hyundai",
+                          modelo : "Getz",
+                          placa  : "POU-102",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Freddy Coronel",
-                email   : "fcoronel@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1234567812"
+                usuario: [
+                    nombre  : "Freddy Coronel",
+                    email   : "fcoronel@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1234567812"
+                ],
+                auto   : [marca  : "Toyota",
+                          modelo : "Fortuner",
+                          placa  : "PBA-3793",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Luz Marina Unda",
-                email   : "lmunda@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1715068159"
+                usuario: [
+                    nombre  : "Luz Marina Unda",
+                    email   : "lmunda@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1715068159"
+                ],
+                auto   : [marca  : "Chevrolet",
+                          modelo : "Aveo",
+                          placa  : "PCQ-8088",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Nelson Jumbo",
-                email   : "njumbo@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1718642174"
+                usuario: [
+                    nombre  : "Nelson Jumbo",
+                    email   : "njumbo@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1718642174"
+                ],
+                auto   : [marca  : "Chevrolet",
+                          modelo : "Esteem",
+                          placa  : "PTE-0730",
+                          tamanio: "P"]
             ],
             [
-                nombre  : "Diego Alcocer",
-                email   : "dalcocer@thoughtworks.com",
-                password: "123".encodeAsSHA256(),
-                esAdmin : false,
-                cedula  : "1234567815"
+                usuario: [
+                    nombre  : "Diego Alcocer",
+                    email   : "dalcocer@thoughtworks.com",
+                    password: "123".encodeAsSHA256(),
+                    esAdmin : false,
+                    cedula  : "1234567815"
+                ],
+                auto   : [marca  : "Ford",
+                          modelo : "F150",
+                          placa  : "PBN-4713",
+                          tamanio: "P"]
             ]
         ]
     }
